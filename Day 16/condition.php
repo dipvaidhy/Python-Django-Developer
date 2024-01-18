5fc863d09d9d019c9ad3c0bdda64e085f92e5e5f
Dipak Vaidh <129586823+dipvaidhy@users.noreply.github.com>
2024-01-18 11:16:24 +0530

    Delete Day 16 directory

--- Day 16/condition.php
+++
@@ -1,9 +0,0 @@
-<?php
-$age = 20;
-
-if ($age < "18") {
-  echo "you can not drive!";
-} else {
-  echo "you can drive!";
-}
-?>
--- Day 16/doWhileloop.php
+++
@@ -1,7 +0,0 @@
-<?php
-$x = 0;
-do{
-    echo "The number is:$x<br>";
-    $x++;
-}while($x<=3);
-?>
